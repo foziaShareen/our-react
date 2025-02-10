1d7b14f42d1806d7bdcedef53a0f0d31deec8e15
foziaShareen <foziashareen1@gmail.com>
2025-02-10 19:44:50 +0500

    form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react';
 import axios from 'axios';
 import Loading from './Loading';
@@ -9,33 +6,64 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users:[],
-      loading:false
+      users: [],
+      loading: false,
+      userCount: 10 // default number of users to load
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  }
-  getUsers(){
-    this.setState({loading:true})
-    axios("https://randomuser.me/api/?results=10")
-    .then((response)  =>
-    this.setState({users: response.data.results
-      ,loading:false})
-    ); 
-    
 
-
-
+  getUsers(count) {
+    this.setState({ loading: true });
+    axios(`https://randomuser.me/api/?results=${count}`)
+      .then((response) =>
+        this.setState({ users: response.data.results, loading: false })
+      );
   }
-  componentWillMount() {
-    this.getUsers()
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers(this.state.userCount);
+    console.log("more users loaded");
   }
-    
+
+  handleInputChange(e) {
+    this.setState({ userCount: e.target.value });
+  }
+
+  componentDidMount() {
+    this.getUsers(this.state.userCount);
+  }
+
   render() {
     return (
       <div>
         <h1>Users List📃</h1>
-        {this.state.loading ? <Loading message="page is loading...⌛"/> : this.state.users.map((user) => <div key={user.email}><h2>{user.name.first} - {user.email}</h2><hr/></div>)}
+        {this.state.loading ? (
+          <Loading message="page is loading...⌛" />
+        ) : (
+          this.state.users.map((user) => (
+            <div key={user.email}>
+              <h2>
+                {user.name.first} - {user.email}
+              </h2>
+              <hr />
+            </div>
+          ))
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="number"
+            value={this.state.userCount}
+            onChange={this.handleInputChange}
+            min="1"
+          />
+          <input value="load users" type="submit" />
+        </form>
       </div>
     );
   }
 }
+
 export default App;
