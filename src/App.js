3e1877dcf42ccf7f4d957285e9ae3f1e894e9759
foziaShareen <foziashareen1@gmail.com>
2025-02-11 10:17:31 +0500

    search applied

--- src/App.js
+++ src/App.js
@@ -1,18 +1,84 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-const App=()=>{
-const [count,setCount]=useState(0);
-const increment=()=>{
-  setCount(count+1)}
-  return(
-    <div> 
-      <h1>Count</h1>
-      <button onClick={increment}>Increment:{count}</button>
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [query, setQuery] = useState('react')
+  const [url,setUrl]=useState(`https://hn.algolia.com/api/v1/search?query=${query}`)
+  const fetchNews = () => {
+    fetch(url)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+
+    .then(data => setNews(data.hits))
+    .catch(err => console.log(err))
+    
+  
+  }
+  const  hadleQuery=(e)=>{
+    e.preventDefault();
+    setQuery(e.target.value)
+
+  }
+  const  hadleSubmit=(e)=>{
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+  }
+  useEffect(() => {
+    fetchNews()
+  }, [url])
+
+
+
+  return (
+    <div>
+      <h1>Users List📃</h1>
+      <input  value={query} onChange={hadleQuery}/>
+      <button onClick={hadleSubmit}>Search</button>
+      {news.map((news) => (
+        <div key={news.title}>
+          <h2>{news.title}</h2>
+          <p>{news.url}</p>
+        </div>
+      ))}
     </div>
   )
 }
 export default App;
+
+
+
+// const App=()=>{
+// const [count,setCount]=useState(0);
+// const [users,setUsers]=useState([]);
+// const increment=()=>{
+//   setCount(count+1)}
+//   // useEffect(()=>{
+//   //   document.title=`title is clicked${count} times`
+
+//   // })
+//   useEffect(()=>{
+//     axios(`https://randomuser.me/api/?results=${count=2}`)
+//     .then((response)=>{
+//       setUsers(response.data.results)
+//       console.log(response.data.results)
+//     })
+//   },[users])
+    
+
+  
+//   return(
+//     <div> 
+//       <p>Users List📃</p>
+//       <h1>Count</h1>
+//       <button onClick={increment}>Increment:{count}</button>
+    
+//     </div>
+//   )
+// }
+
+// export default App;
 
 
 
@@ -88,4 +154,4 @@ export default App;
 //   }
 // }
 
-// export default App;
+
